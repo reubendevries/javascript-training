e4f93f40a85251276d23efef978e15fe9afde0d6
Reuben deVries <reubendevries@gmail.com>
2024-03-18 15:27:56 -0700

    section-01/hello-world -------------------------------------------------------------------
    - Added exercises through Hello, World!
    
    -------------------------------------------------------------------
    Signed-off-by: Reuben deVries <reubendevries@gmail.com>

---
+++ hello_world.js
@@ -0,0 +1,12 @@
+alert("Hello, World!");
+
+let js = "Amazing";
+
+if (js === "Amazing") alert("JavaScript is FUN!");
+
+js = "boring"
+
+if (js === "Amazing") alert("Javascript is FUN!");
+
+let sum = 40 + 8 + 23 - 10
+console.log(sum);
