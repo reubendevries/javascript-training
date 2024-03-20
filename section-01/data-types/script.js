07d5c34ad35d463408775104a495ece7a3d56cd4
Reuben deVries <reubendevries@gmail.com>
2024-03-19 20:24:46 -0700

    section-01/data-types ------------------------------------------------------------------
    - completed lesson in section 01 on data-types.
    
    Signed-off-by: Reuben deVries <reubendevries@gmail.com>

---
+++ section-01/data-types/script.js
@@ -0,0 +1,37 @@
+// number: floating point number -> Used for decimals and intergers
+let age = 23;
+// string: sequence of characters -> used for text
+let firstName = "Jonas";
+// boolean: logical type that can only be true of false -> used for taking decisions
+let fullAge = true;
+// undefined: value taken by a variable that is not yet defined.
+
+// null: also means 'emtpy value'
+
+// symbol: Value that is unique and cannot be changed (immutable)
+
+// BigInt: Larger integers than the Number type can hold.
+
+// Javascript has dynamic typing. We do not have to manually define the data type of the value stored in a variable. Instead, data typres are determined automatically.
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = 'YES!';
+
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
